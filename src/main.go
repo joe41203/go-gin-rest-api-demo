ee9b5e7bc86e661b44e2f0bb623cf8b129535228
joe <kinjoyuji@gmail.com>
2021-11-04 15:18:43 +0900

    get parameter test

--- src/main.go
+++ src/main.go
@@ -14,11 +14,22 @@ func Post(c *gin.Context) {
 	})
 }
 
+func QueryParameter(c *gin.Context) {
+	name := c.Query("name")
+	age := c.Query("age")
+
+	c.JSON(200, gin.H{
+		"name": name,
+		"age":  age,
+	})
+}
+
 func main() {
 	server := gin.Default()
 
 	server.GET("/", Home)
 	server.POST("/post", Post)
+	server.GET("/query", QueryParameter)
 
 	server.Run()
 }
